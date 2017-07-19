a452c93c6b7c440a57f52a11bf5db41f599f8f59
AnnYevlakova <ann.yevlakova@gmail.com>
2017-07-19 17:31:15 +0300

    added simple react-router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
-const title = 'My Minimal React Webpack Babel Setup';
+class Home extends Component {
+	render() {
+		return (
+			<div>
+				<p>home</p>
+			</div>
+		);
+	}
+}
+class Login extends Component {
+	render() {
+		return (
+			<div>
+				<p>Login</p>
+			</div>
+		);
+	}
+}
+class Registration extends Component {
+	render() {
+		return (
+			<p>Registration</p>
+		);
+	}
+}
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<header>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/login'>Login</Link></li>
+            <li><Link to='/registration'>Registration</Link></li>
+          </ul>
+				</header>
+        <main>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/login' component={Login}/>
+            <Route path='/registration' component={Registration}/>
+          </Switch>
+        </main>
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(
-	<div>{title}</div>,
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
 	document.getElementById('app')
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,38 @@
 module.exports = {
-		entry: [
-				'webpack-dev-server/client?http://localhost:8080',
-				'webpack/hot/only-dev-server',
-				'./src/index.js'
-		],
-		module: {
-				rules: [
-						{
-								test: /\.js$/,
-								exclude: /node_modules/,
-								loader: 'eslint-loader'
-						},
-				],
-				loaders: [{
-						test: /index.js?$/,
-						exclude: /node_modules/,
-						loader: 'react-hot-loader!babel-loader'
-				}/*,
-				{
-						test: /\.js$/,
-						exclude: /node_modules/,
-						loader: 'eslint-loader'
-				}*/
-				]
+  entry: [
+	'webpack-dev-server/client?http://localhost:8080',
+	  'webpack/hot/only-dev-server',
+	  './src/index.js'
+	  ],
+	module: {
+	  rules: [
+		{
+		  test: /\.js$/,
+		  exclude: /node_modules/,
+		  loaders: ['babel-loader', 'eslint-loader']
 		},
-		resolve: {
-				extensions: ['*', '.js', '.jsx']
-		},
-		output: {
-				path: __dirname + '/dist',
-				publicPath: '/',
-				filename: 'bundle.js'
-		},
-		devServer: {
-				contentBase: './dist',
-				hot: true
-		}
+	  ],
+	  loaders: [{
+	    test: /\.(js|jsx)$/,
+		exclude: /node_modules/,
+		loaders: ['react-hot-loader!babel-loader'],
+		query:
+		  {
+			presets: ["react"]
+		  }
+	  }
+	  ]
+	},
+	resolve: {
+	  extensions: ['*', '.js', '.jsx']
+	},
+	output: {
+	  path: __dirname + '/dist',
+	  publicPath: '/',
+	  filename: 'bundle.js'
+	},
+	devServer: {
+	  contentBase: './dist',
+	  hot: true
+	}
 };
