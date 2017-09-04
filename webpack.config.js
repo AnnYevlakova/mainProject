2d1211bd2d8d7ea608544312b2f784a97a498bda
AnnYevlakova <ann.yevlakova@gmail.com>
2017-09-04 14:30:06 +0300

    fixed creating store

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     devtool: "eval-source-map",
     entry: [
-        "webpack-dev-server/client?http://localhost:8080",
+        "webpack-dev-server/client?",
         "webpack/hot/only-dev-server",
         "./src/index.js",
     ],
